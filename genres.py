1144a6069afc8d4c506294e751563faeba2384e0
Juri Dispan <juri.dispan@uni-ulm.de>
2022-06-21 15:18:49 +0200

    sorting

--- genres.py
+++ genres.py
@@ -13,13 +13,13 @@ l_tr = 27742
 l_val = 60486
 
 
-def make_cm(res):
+def make_cm(vector, res):
     fig = plt.figure()
     # fig.suptitle("haha")
     gs = fig.add_gridspec(2, 2, hspace=0.5, wspace=0.5)
     ax = gs.subplots()
     i = 0
-    for x, m in sorted(res.items(), key=lambda y: y[1]["F1"]):
+    for x, m in sorted(vector.items(), key=lambda y: res[y[0]]["F1"]):
         if x == "InvariantsMiner":
             x = "Invariants Mining"
         axis = ax[i % 2, i // 2]
@@ -46,13 +46,13 @@ def make_cm(res):
     plt.savefig(f"/lustre/work/ws/ws1/ul_csu94-test/Graphics/ad-all.svg")
 
 def output_t1(res):
-    for x, m in res.items():
+    for x, m in sorted(res.items(), key=lambda y: res[y[0]]["F1"]):
         if x == "InvariantsMiner":
             x = "Invariants Mining"
         print(f"{x:17}&${m['Precision'][0]:.3f}$&${m['Recall'][0]:.3f}$&${m['F1'][0]:.3f}$&{'?SI{'}{1000*m['t_train'][0]/l_tr:.3f}{'}{?milli?second}'}&{'?SI{'}{1000*m['t_predict'][0]/(l_val):.3f}{'}{?milli?second}??'}".replace("?", "\\"))
 
-def output_t2(vector):
-    for x, m in vector.items():
+def output_t2(vector, res):
+    for x, m in sorted(vector.items(), key=lambda y: res[y[0]]["F1"]):
         if x == "InvariantsMiner":
             x = "Invariants Mining"
         m = confusion_matrix(m["y_true"], m["y_pred"])
@@ -66,7 +66,7 @@ for m in models:
         js = json.load(f)
     vectors[m] = js
     results[m] = pd.read_csv(f"benchmarks/benchmark_result_{m}.csv")
-make_cm(vectors)
+make_cm(vectors, results)
 output_t1(results)
 print()
-output_t2(vectors)
+output_t2(vectors, results)
