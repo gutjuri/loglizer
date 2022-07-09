e2bc18e4e70d587cf3cc0104510a49d1fa3aa9dc
Juri Dispan <juri.dispan@uni-ulm.de>
2022-07-09 13:00:36 +0200

    include im

--- genres.py
+++ genres.py
@@ -9,7 +9,7 @@ from sklearn.metrics import (
 import pandas as pd
 
 models = ["PCA", "LogCluster", "InvariantsMiner", "DeepLog"]
-models  =["PCA", "LogCluster"]
+models  =["PCA", "LogCluster", "InvariantsMiner"]
 l_tr = 27742
 l_val = 60486
 
